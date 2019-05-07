e641163b2ed43c3b9ffa0084039d9977f7076813
Ricardo Saucedo <ricardo.saucedo@slu.edu>
2019-05-07 11:28:31 -0500

    update of twitter script, has skeleton for two w2v

--- twitter_senti.py
+++ twitter_senti.py
@@ -38,7 +38,7 @@ from keras.layers import Activation, Dense
 def ingest(filename):
 	#reads in csv files of tweets and their respective sentiment values
 	#
-	data = pd.read_csv(filename, engine='python')
+	data = pd.read_csv(filename, engine='python', error_bad_lines=False)
 	data.drop(['ItemID', 'Date','Blank','SentimentSource'], axis=1, inplace=True)
 	data = data[data.Sentiment.isnull() == False]
 	data['Sentiment'] = data['Sentiment'].map({4:1,0:0})
@@ -129,47 +129,36 @@ if __name__ == '__main__':
 	n_dim = 200
 	n = 120000
 	min_count = 10
-
+	filename1 = './GayTwitterTweets.csv'
 	filename2 = './tweets2.csv'
 
+	data1 = ingest(filename1)
+	data1 = postprocess(data1, n)
 
 	data2 = ingest(filename2)
 	data2 = postprocess(data2, n)
-	#x_train, x_test, y_train, y_test, = train_test_split(np.array(data.head(n).tokens), np.array(data.head(n).Sentiment), test_size = 0.3)
+	x_train, x_test, y_train, y_test, = train_test_split(np.array(data1.head(n).tokens), np.array(data1.head(n).Sentiment), test_size = 0.3)
 
 
 	x_train2, x_test2, y_train2, y_test2, = train_test_split(np.array(data2.head(n).tokens), np.array(data2.head(n).Sentiment), test_size = 0.3)
 
-	#x_train = labelizeTweets(x_train, 'TRAIN')
-	#x_test = labelizeTweets(x_test, 'TEST')
+	x_train = labelizeTweets(x_train, 'TRAIN')
+	x_test = labelizeTweets(x_test, 'TEST')
 
 	
 	x_train2 = labelizeTweets(x_train2, 'TRAIN')
 	x_test2 = labelizeTweets(x_test2, 'TEST')
 
 	
-#	tweet_w2v = Word2Vec(size = n_dim, min_count=10)
-#	tweet_w2v.build_vocab([x.words for x in tqdm(x_train)])
-#	tweet_w2v.train([x.words for x in tqdm(x_train)],total_examples=tweet_w2v.corpus_count,epochs=tweet_w2v.iter)
+	tweet_w2v = Word2Vec(size = n_dim, min_count=5)
+	tweet_w2v.build_vocab([x.words for x in tqdm(x_train)])
+	tweet_w2v.train([x.words for x in tqdm(x_train)],total_examples=tweet_w2v.corpus_count,epochs=tweet_w2v.iter)
 	
-	tweet_w2v2 = Word2Vec(size = n_dim, min_count=5)
+	tweet_w2v2 = Word2Vec(size = n_dim, min_count=10)
 	tweet_w2v2.build_vocab([x.words for x in tqdm(x_train2)])
 	tweet_w2v2.train([x.words for x in tqdm(x_train2)],total_examples=tweet_w2v2.corpus_count,epochs=tweet_w2v2.iter)
 	
-	#try:
-	#print(tweet_w2v['the'])
-	#print(tweet_w2v.most_similar('gay'))
-	#print(tweet_w2v.most_similar('queer'))
-	#print(tweet_w2v.most_similar('homosexual'))
-	#print(tweet_w2v.most_similar('fag'))
-	#print(tweet_w2v.most_similar('homo'))
-	#print(tweet_w2v.most_similar('lesbian'))
-	#print(tweet_w2v.most_similar('dyke'))
-	#ABprint(tweet_w2v.most_similar('transgender'))
-	#print(tweet_w2v.most_similar('trans'))
-	#print(tweet_w2v.most_similar('lgbt'))
-	#tweet_w2v.most_similar('bar')
-	#tweet_w2v.most_similar('the')
+	
 	
 	import bokeh.plotting as bp
 	from bokeh.models import HoverTool, BoxSelectTool
@@ -181,8 +170,12 @@ if __name__ == '__main__':
 	#plot_tfidf = bp.figure(plot_width=800, plot_height=800, title="A map of \d word vectors"%len(word_vectors),tools="pan,wheel_zoom,box_zoom,reset,hover,previewsave",x_axis_type=None, y_axis_type=None, min_border=1)
 
 	#getting a list of word vectors. limit to 10000. each is of 200 dimension
-	word_vectors = [tweet_w2v2[w] for w in tweet_w2v2.wv.vocab.keys()]
+	word_vectors = [tweet_w2v[w] for w in tweet_w2v.wv.vocab.keys()]
+
+	word_vectors2 = [tweet_w2v2[w] for w in tweet_w2v2.wv.vocab.keys()]
 	print(len(word_vectors))
+	print(len(word_vectors2))
+
 	plot_tfidf = bp.figure(plot_width=800, plot_height=800, title="A map of 11000 word vectors",tools="pan,wheel_zoom,box_zoom,reset,hover,previewsave",x_axis_type=None, y_axis_type=None, min_border=1)
 
 	#dimensionality reduction. converting the vectors to 2d vectors
@@ -190,9 +183,16 @@ if __name__ == '__main__':
 	tsne_model = TSNE(n_components=2, verbose=1, random_state=0)
 	tsne_w2v = tsne_model.fit_transform(word_vectors)
 
+	tsne_model2 = TSNE(n_components=2, verbose=1, random_state=0)
+	tsne_w2v2 = tsne_model.fit_transform(word_vectors2)
+
+
 	#putting everything in a dataframe
 	tsne_df = pd.DataFrame(tsne_w2v, columns=['x','y'])
-	tsne_df['words'] = tweet_w2v2.wv.vocab.keys()
+	tsne_df['words'] = tweet_w2v.wv.vocab.keys()
+
+	tsne_df2 = pd.DataFrame(tsne_w2v2, columns =['x','y'])
+	tsne_df2['words'] = tweet_w2v2.wv.vocab.keys()
 
 	#plotting the corresponding word appearts when you hover on the data point.
 	plot_tfidf.scatter(x='x', y='y', source=tsne_df)
@@ -203,13 +203,36 @@ if __name__ == '__main__':
 	
 	if True:
 
-		print ('building tf-idf matrix ...')
+		print ('building tf-idf matrix for GayTwitter tweets...')
+		vectorizer = TfidfVectorizer(analyzer=lambda x: x, min_df=10)
+		matrix = vectorizer.fit_transform([x.words for x in x_train])
+		tfidf = dict(zip(vectorizer.get_feature_names(), vectorizer.idf_))
+		print ('vocab size :', len(tfidf))
+
+		from sklearn.preprocessing import scale
+
+		train_vecs_w2v = np.concatenate([buildWordVector(z, n_dim) for z in tqdm(map(lambda x: x.words, x_train))])
+		train_vecs_w2v = scale(train_vecs_w2v)
+		test_vecs_w2v = np.concatenate([buildWordVector(z, n_dim) for z in tqdm(map(lambda x: x.words, x_test))])
+		test_vecs_w2v = scale(test_vecs_w2v)
+
+		model = Sequential()
+		model.add(Dense(32, activation='relu', input_dim=200))
+		model.add(Dense(1, activation='sigmoid'))
+		model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+
+		model.fit(train_vecs_w2v, y_train, epochs=9, batch_size=32, verbose=2)
+
+		score = model.evaluate(test_vecs_w2v, y_test, batch_size=64, verbose=2)
+		print (score[1])
+
+		print ('building tf-idf matrix for normal tweets...')
 		vectorizer = TfidfVectorizer(analyzer=lambda x: x, min_df=10)
 		matrix = vectorizer.fit_transform([x.words for x in x_train2])
 		tfidf = dict(zip(vectorizer.get_feature_names(), vectorizer.idf_))
 		print ('vocab size :', len(tfidf))
 
-		from sklearn.preprocessing import scale
+
 		train_vecs_w2v2 = np.concatenate([buildWordVector(z, n_dim) for z in tqdm(map(lambda x: x.words, x_train2))])
 		train_vecs_w2v2 = scale(train_vecs_w2v2)
 		test_vecs_w2v2 = np.concatenate([buildWordVector(z, n_dim) for z in tqdm(map(lambda x: x.words, x_test2))])
